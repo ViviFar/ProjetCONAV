741357b16fa22fc695c4aa0f484ae0e8f9296012
paraston <>
2019-03-01 11:42:08 +0100

    Scene 2050 + années 80 + Impl Vive

---
+++ CONAV/Introduction To HTC Vive - Starter/Assets/Scripts/80s/Drawer_constraint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer_constraint : MonoBehaviour {
+
+    public Transform Limit_transform;
+    private float limit_position;
+    private float drawer_position;
+	// Use this for initialization
+	void Start () {
+        limit_position = Limit_transform.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        drawer_position = transform.position.x;
+        if (drawer_position - limit_position > 0.371f)
+            drawer_position = limit_position + 0.371f;
+	}
+}
